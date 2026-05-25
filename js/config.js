510ce290e74215efb9e3c677247cd053551e6d39
bamdyasin <ba.mdyasin@gmail.com>
2026-05-26 02:05:20 +0600

    coinstorebd.com

---
+++ js/config.js
@@ -0,0 +1,20 @@
+const paymentMethods = {
+    bkash: {
+        dropdownLabel: "বিকাশ (মার্চেন্ট)",
+        type: "বিকাশ মার্চেন্ট",
+        number: "01845464034"
+    },
+    nagad: {
+        dropdownLabel: "নগদ (পার্সোনাল)",
+        type: "নগদ পার্সোনাল",
+        number: "01845464034"
+    }
+};
+
+const socialLinks = {
+    facebook: "https://facebook.com/coinstorebd",
+    whatsapp: "https://wa.me/8801845464034",
+    tiktok: "https://tiktok.com/@coinstore.bd",
+    youtube: "https://youtube.com/@coinstorebd",
+    videoUrl: "https://youtube.com/@coinstorebd"
+};
